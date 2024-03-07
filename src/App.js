1f51f8b7489b85b2179f27598dc9903fe9468495
awesomecode <ankithh354@gmail.com>
2024-03-07 05:50:08 +0530

    MUSIC PLAYER

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggleSidebar}>Open Sidebar</button>
+      <Sidebar isOpen={isOpen} toggle={toggleSidebar} />
+      {/* Other components */}
     </div>
   );
 }
