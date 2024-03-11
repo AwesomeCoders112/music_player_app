81ef685a85401fa194cbf8552504b6048a45d5b5
awesomecode <ankithh354@gmail.com>
2024-03-11 05:59:05 +0530

    Add Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Sidebar from './components/Sidebar';
 
+
 function App() {
- const [isOpen, setIsOpen] = useState(false);
-
- const toggleSidebar = () => {
-    setIsOpen(!isOpen);
- };
-
  return (
     <div className="App">
-      <button className="mybtn" onClick={toggleSidebar}>Open Sidebar</button>
-      <Sidebar isOpen={isOpen} toggle={toggleSidebar} />
+      <Sidebar />
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <img src='C:\Users\Ankith Hebbar\clone\src\unnamed.png' alt='logo' style={{ width: '800px', height: 'auto' }} />
+        <img src='./Back.jpg' alt='logo' style={{ width: '800px', height: 'auto' }} />
         <h1 style={{ color: 'black', textAlign: 'center', paddingTop: '5%', fontSize: '2.5em' }}>Welcome to Music Player</h1>
       </div>
       {/* Other components */}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import './Sidebar.css'; // Make sure to create and import the Sidebar.css file
+import unnamed from './unnamed.png'; // Replace this with the correct path to your logo image
+import './Sidebar.css'; // Ensure this CSS file exists and contains styles for the sidebar
 
-const Sidebar = ({ isOpen, toggle }) => {
- return (
-    <div className={isOpen ? "sidebar open" : "sidebar"}>
-      <button onClick={toggle}>Close</button>
-      {/* Removed the image tag to ensure it doesn't appear in the sidebar */}
-      <ul>
-        <li>Home</li>
-        <li>Search</li>
-        <li>Your Libraries</li>
-        <li>Create Playlists</li>
-      </ul>
-      <br></br>
-      <br></br>
-      <ul>
-        <li> Song 1 </li>
-        <li> Song 2 </li>
-        <li> Song 3 </li>
-      </ul>
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <img src={unnamed} alt="Logo" className="sidebar-logo" />
+      <div className="sidebar-list-container">
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Your Libraries</li>
+          <li>Create Playlists</li>
+        </ul>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
     </div>
- );
+  );
 };
 
 export default Sidebar;
