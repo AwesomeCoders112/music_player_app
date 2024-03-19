106d9140567e00f8a2f4e21464e9bce65c3c83ba
awesomecode <ankithh354@gmail.com>
2024-03-19 22:49:13 +0530

    Added Player

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import Search from './components/Search';
 import back from './Back.jpg'
+import PlayerControls from './components/Control';
 
 function App() {
  return (
     <Router>
       <div className="App">
         <Sidebar />
+        <PlayerControls/>
         <Routes>
           <Route path="/" element={
             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
               <img src={back} alt='logo' style={{ maxWidth: '100%', height: '0%', backgroundBlendMode: 'multiply' }} />
               <div className='mycls' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'yellow' }}>
-                <h1 style={{ fontSize: '4.5em' }}>Welcome to Music Player</h1>
+                <h1 style={{ fontSize: '4.5em'}}>Welcome to Music Player</h1>
+                <h1 style={{ fontSize: '2.5em'}}>Your one stop Destination to all music</h1>
                 <ul style={{ listStyleType: 'none', padding: 0 }}>
                 </ul>
               </div>
---
+++ src/components/Control.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { BsFillPlayCircleFill, BsFillPauseCircleFill, BsShuffle } from "react-icons/bs";
+import { CgPlayTrackNext, CgPlayTrackPrev } from "react-icons/cg";
+import { FiRepeat } from "react-icons/fi";
+
+export default function PlayerControls() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const handlePrevTrack = () => {
+    // Handle previous track logic here
+    console.log("Previous track");
+  };
+
+  const handleNextTrack = () => {
+    // Handle next track logic here
+    console.log("Next track");
+  };
+
+  return (
+    <Container>
+      <div className="shuffle">
+        <BsShuffle />
+      </div>
+      <div className="previous">
+        <CgPlayTrackPrev onClick={handlePrevTrack} />
+      </div>
+      <div className="state">
+        {isPlaying ? (
+          <BsFillPauseCircleFill onClick={togglePlayPause} />
+        ) : (
+          <BsFillPlayCircleFill onClick={togglePlayPause} />
+        )}
+      </div>
+      <div className="next">
+        <CgPlayTrackNext onClick={handleNextTrack} />
+      </div>
+      <div className="repeat">
+        <FiRepeat />
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  gap: 2rem;
+  svg {
+    color: #b3b3b3;
+    transition: 0.2s ease-in-out;
+    &:hover {
+      color: black;
+    }
+  }
+  .state {
+    svg {
+      color: black;
+    }
+  }
+  .previous,
+  .next,
+  .state {
+    font-size: 2rem;
+  }
+`;
