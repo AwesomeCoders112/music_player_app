d4cb9b956cfea759ce90cdda7f89c16e3f66803d
awesomecode <ankithh354@gmail.com>
2024-04-03 19:48:43 +0530

    Some Mods1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Search from './components/Search';
+import Sidebar from './components/SideBar/Sidebar';
+import Search from './components/Search/Search';
 import back from './back.jpg'
 
 import PlayerControls from './components/Control';
-import Playlist from './components/Playlists';
-import Playlist1 from './components/Playlist1';
-import Playlist2 from './components/Playlist2';
-import Header from './components/Header';
-import Home from './components/Home';
+import Playlist from './components/Library/Playlists';
+import Playlist1 from './components/Playlist/Playlist1';
+import Playlist2 from './components/Playlist/Playlist2';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import AuthForm from './components/AuthForm/AuthForm';
 
 function App() {
